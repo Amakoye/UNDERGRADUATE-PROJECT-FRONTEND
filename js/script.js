b6878ce4035a75eba7ac24be52462a67325ee091
Amakoye <charlesamakoye@gmail.com>
2020-11-15 12:45:50 +0300

    refactored to use data from API

--- js/script.js
+++ js/script.js
@@ -1,123 +1,160 @@
-// labels on the x-axis
-cereals = ["Maize", "Beans", "Soya", "Millet", "Sorghum"];
-cerealsData = [12, 19, 3, 5, 2];
-vegs = ["Spinach", "Cabbages", "Tomatoes", "Onions", "Capsicum"];
-vegData = [10, 12, 5, 8, 13];
+/* import axios from "axios"; */
+/* console.log("printing data to the console"); */
 
-var cerealBar_gragh = document.getElementById("cereal-bar").getContext("2d");
-var cereal_bar = new Chart(cerealBar_gragh, {
-  type: "bar",
-  data: {
-    labels: cereals,
-    datasets: [
-      {
-        label: "Statistical demand for individual cereals",
-        data: cerealsData,
-        backgroundColor: "#def",
-        borderColor: "rgba(54, 162, 235, 1)",
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
+const api = {
+  base_url: "https://rocqjones.pythonanywhere.com/api/products",
+};
+
+//Get fruits data from the api
+const getFruits = async () => {
+  try {
+    await fetch(`${api.base_url}=fruits`)
+      .then((data) => {
+        return data.json();
+      })
+      .then(displayFruitData);
+  } catch (error) {
+    alert("Oops! try again later");
+  }
+};
+
+const displayFruitData = (data) => {
+  let fruits = data.map((item) => item.product_name);
+  let fruitsData = data.map((item) => item.demand);
+
+  //render data to the page
+  let fruitsBarGraph = document.getElementById("fruit-bar").getContext("2d");
+  let label = "fruits";
+  let fruitsLineGraph = document
+    .getElementById("fruit-line-graph")
+    .getContext("2d");
+
+  //call methods to draw the graphs
+  drawBarGraph(fruitsBarGraph, fruits, fruitsData, label);
+  drawLineGraph(fruitsLineGraph, fruits, fruitsData, label);
+  /*  console.log(fruits);
+  console.log(fruitsData); */
+};
+
+// Get Vegetables from the api
+const getVegetables = async () => {
+  try {
+    await fetch(`${api.base_url}=vegetables`)
+      .then((data) => {
+        return data.json();
+      })
+      .then(displayVegData);
+  } catch (error) {
+    alert("Oops! try again later");
+  }
+};
+
+const displayVegData = (data) => {
+  let vegetables = data.map((item) => item.product_name);
+  let vegData = data.map((item) => item.demand);
+
+  //render data to the page
+  let vegBarGraph = document.getElementById("veg-bar").getContext("2d");
+  let label = "vegetables";
+  let vegLineGraph = document.getElementById("veg-line-graph").getContext("2d");
+
+  //call methods to draw the graphs
+  drawBarGraph(vegBarGraph, vegetables, vegData, label);
+  drawLineGraph(vegLineGraph, vegetables, vegData, label);
+  /* console.log(vegetables);
+  console.log(vegData); */
+};
+
+// Get Cereals from the api
+const getCereals = async () => {
+  try {
+    await fetch(`${api.base_url}=cerials`)
+      .then((data) => {
+        return data.json();
+      })
+      .then(displayCerealData);
+  } catch (error) {
+    alert("Oops! try again later");
+  }
+};
+
+const displayCerealData = (data) => {
+  let cereals = data.map((item) => item.product_name);
+  let cerealsData = data.map((item) => item.demand);
+
+  //render data to the page
+  let cerealBarGraph = document.getElementById("cereal-bar").getContext("2d");
+  let label = "cereals";
+  let cerealLineGraph = document
+    .getElementById("cereal-line-graph")
+    .getContext("2d");
+
+  //call methods to draw the graphs
+  drawBarGraph(cerealBarGraph, cereals, cerealsData, label);
+  drawLineGraph(cerealLineGraph, cereals, cerealsData, label);
+  /*  console.log(cereals);
+  console.log(cerealsData); */
+};
+
+// draw bar-graph
+const drawBarGraph = (graphName, dataName, dataSet, dataLabel) => {
+  let bar_graph = new Chart(graphName, {
+    type: "bar",
+    data: {
+      labels: dataName,
+      datasets: [
         {
-          ticks: {
-            beginAtZero: true,
-          },
+          label: `Statistical demand for individual ${dataLabel}`,
+          data: dataSet,
+          backgroundColor: "#def",
+          borderColor: "rgba(54, 162, 235, 1)",
+          borderWidth: 1,
         },
       ],
     },
-  },
-});
-
-var vegBar_gragh = document.getElementById("veg-bar").getContext("2d");
-var veg_bar = new Chart(vegBar_gragh, {
-  type: "bar",
-  data: {
-    labels: vegs,
-    datasets: [
-      {
-        label: "Statistical demand for individual cereals",
-        data: vegData,
-        backgroundColor: "#def",
-        borderColor: "rgba(54, 162, 235, 1)",
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
           },
+        ],
+      },
+    },
+  });
+};
+
+// draw line graph
+const drawLineGraph = (graphName, dataName, dataSet, dataLabel) => {
+  let line_graph = new Chart(graphName, {
+    type: "line",
+    data: {
+      labels: dataName,
+      datasets: [
+        {
+          label: `Statistical demand for individual ${dataLabel}`,
+          data: dataSet,
+          backgroundColor: "#def",
+          borderColor: "rgba(54, 162, 235, 1)",
+          borderWidth: 1,
         },
       ],
     },
-  },
-});
-
-// line-graphs
-
-/* Cereals */
-var cerealLine_gragh = document
-  .getElementById("cereal-line-graph")
-  .getContext("2d");
-var cereal_line = new Chart(cerealLine_gragh, {
-  type: "line",
-  data: {
-    labels: cereals,
-    datasets: [
-      {
-        label: "General Statistical demand for all cereals",
-        data: cerealsData,
-        backgroundColor: "#def",
-        borderColor: "rgba(54, 162, 235, 1)",
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
           },
-        },
-      ],
-    },
-  },
-});
-
-/* Vegetables */
-var vegLine_gragh = document.getElementById("veg-line-graph").getContext("2d");
-var cereal_line = new Chart(vegLine_gragh, {
-  type: "line",
-  data: {
-    labels: vegs,
-    datasets: [
-      {
-        label: "General Statistical demand for all cereals",
-        data: vegData,
-        backgroundColor: "#def",
-        borderColor: "rgba(54, 162, 235, 1)",
-        borderWidth: 1,
+        ],
       },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-        },
-      ],
     },
-  },
-});
+  });
+};
+getFruits();
+getCereals();
+getVegetables();
